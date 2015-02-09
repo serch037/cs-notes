4b35cf395827151eba8e6061ec68b998b64d1bf4
serch037@gmail.com <sergio@localhost.localdomain>
2015-02-08 21:43:23 -0600

    First publish

--- pelicanconf.py
+++ pelicanconf.py
@@ -26,7 +26,7 @@ AUTHOR_FEED_RSS = None
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
 STATIC_PATHS = ['images']
 TYPOGRIFY = True
 MD_EXTENSIONS =  [ 'toc', 'codehilite','extra']
